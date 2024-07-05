a4f3728b67a34bff9ab7c4234b055ea8ce33b317
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-07-05 07:09:25 +0000

    [Add]deviseモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :makers, skip: [:passwords], controllers: {
+    registrations: 'seller/registrations',
+    sessions: 'seller/sessions'
+  }
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: 'consumer/registrations',
+    sessions: 'consumer/sessions'
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
