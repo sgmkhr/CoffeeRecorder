1311dec80122e3666df2b69cab0b4a1b2f3ef246
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-07-10 12:44:21 +0000

    [Add]トップ画面、enum途中

--- app/javascript/packs/tabs.js
+++ app/javascript/packs/tabs.js
@@ -1,1 +1,8 @@
 /*global $*/
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
+});
