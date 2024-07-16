6579080a3058fc3c4859e00519cf5b227fc21726
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-07-16 10:39:00 +0000

    [Add]ページトップスクロールボタン

---
+++ app/javascript/script.js
@@ -0,0 +1,12 @@
+/*global $*/
+document.addEventListener("turbolinks:load", function() {
+ 
+  // ページトップへスクロールするボタンを押した時
+  $('#back_to_pagetop').on('click', function(event) {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 800);
+    event.preventDefault();
+  });
+  
+});
