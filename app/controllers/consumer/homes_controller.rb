361e6c3a5145381851c3d3acf29c5673594d6cc3
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-07-26 09:53:57 +0000

    [Add]レイアウト調整

--- app/controllers/consumer/homes_controller.rb
+++ app/controllers/consumer/homes_controller.rb
@@ -2,11 +2,11 @@ class Consumer::HomesController < ApplicationController
   before_action :authenticate_user!
   def top
     @record = Record.new
-    @records = current_user.records.page(params[:page])
+    @records = current_user.records.page(params[:page]).per(4)
     
-    @preference_data = current_user.get_coffee_preference_data_array
+    @preference_data = current_user.get_coffee_preference_data_array if @records.present?
     
-    @coffees = Kaminari.paginate_array(current_user.get_matching_coffee_posts).page(params[:coffee_posts_page])
+    @coffees = Kaminari.paginate_array(current_user.get_matching_coffee_posts).page(params[:coffee_posts_page]).per(4)
     
     @current_tab = params[:current_tab]
     @current_tab == "main_tab" unless @current_tab.present?
