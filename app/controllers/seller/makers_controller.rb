a4ed62de5d08b3ddf67deb392e04bd2bffd12c64
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-07-27 09:30:42 +0000

    [Add]ph１実装完了

--- app/controllers/seller/makers_controller.rb
+++ app/controllers/seller/makers_controller.rb
@@ -4,24 +4,24 @@ class Seller::MakersController < ApplicationController
   def edit
     @maker = current_maker
   end
-  
+
   def update
     @maker = Maker.find(params[:id])
     if @maker.update(maker_params)
-      redirect_to seller_edit_maker_path(@maker.id), notice: I18n.t("seller.makers.update.notice") #後ほどルート先修正
+      redirect_to seller_shops_path, notice: I18n.t("seller.makers.update.notice")
     else
       flash.now[:alert] = I18n.t("seller.makers.update.alert")
-      render seller_edit_maker_path
+      render :edit
     end
   end
-  
+
   def destroy
     Maker.find(params[:id]).destroy
     redirect_to new_maker_registration_path, notice: I18n.t("seller.makers.destroy.notice")
   end
-  
+
   private
-  
+
   def maker_params
     params.require(:maker).permit(:email, :company_name)
   end
