d6d989517302becde9e05ee6a4cb871febb292ff
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-07-28 14:14:00 +0000

    [Fix]ページトップボタン修正

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,12 +1,12 @@
 /*global $*/
 document.addEventListener("turbolinks: load", function() {
- 
+
   // ページトップへスクロールするボタンを押した時
-  $('#back_to_pagetop').on('click', function(event) {
+  $('#btn_to_pagetop').on('click', function(event) {
     $("body, html").animate({
       scrollTop: 0
     }, 800);
     event.preventDefault();
   });
-  
+
 });
