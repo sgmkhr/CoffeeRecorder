a2650f8350dc3e45a67f7ec55cf7df4b72823e31
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-08-02 08:50:44 +0000

    [Add]recordsソート機能

--- app/controllers/consumer/homes_controller.rb
+++ app/controllers/consumer/homes_controller.rb
@@ -2,7 +2,14 @@ class Consumer::HomesController < ApplicationController
   before_action :authenticate_user!
   def top
     @record = Record.new
-    @records = current_user.records.page(params[:page]).per(6)
+    
+    @sort = params[:sort]
+    @records = current_user.records
+    @records = @records.latest    if @sort == "latest"
+    @records = @records.old       if @sort == "old"
+    @records = @records.high_rate if @sort == "high_rate"
+    @records = @records.low_rate  if @sort == "low_rate"
+    @records = @records.page(params[:page]).per(6)
     
     @preference_data = current_user.get_coffee_preference_data_array if @records.present?
     
